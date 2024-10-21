19218360ea6021e93275f72c96cdf2fd6b44d5b8
Francis Waihiga <francisgwaihiga@gmail.com>
2024-10-21 15:26:16 +0300

    Output error correcred by adding s type hint

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random delay between 0 and max_delay seconds
     and then return it."""
     # Generate a random float between 0 and max_delay

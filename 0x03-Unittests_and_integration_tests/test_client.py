4f3c4a590399aad62b8909e05add869041446c2e
Francis Waihiga <francisgwaihiga@gmail.com>
2024-11-09 21:43:24 +0300

    Task 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""A github org client unittest
+"""
+import unittest
+from client import GithubOrgClient
+from parameterized import parameterized
+from unittest.mock import patch
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test GithubOrgClient"""
+    @parameterized.expand([
+        'google',
+        'abc'
+    ])
+    @patch('client.get_json')
+    def test_org(self, mock_get_json, org_name):
+        """Test GithubOrgClient.org"""
+        client = GithubOrgClient(org_name)
+        client.org()
+        mock_get_json.called_with_once(client.ORG_URL.format(org=org_name))

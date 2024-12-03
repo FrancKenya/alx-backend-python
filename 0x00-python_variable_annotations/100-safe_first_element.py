07f6622f8119c38e0c808bced600604863af29e3
Francis Waihiga <francisgwaihiga@gmail.com>
2024-12-03 23:09:32 +0300

    output error corrected

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
-"""
-This module provides duck-typed function
-"""
-from typing import Sequence, TypeVar, Optional
-
-T = TypeVar('T')
+""" """
+from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[T]) -> Optional[T]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
     Returns the first element of a sequence if it exists,
     otherwise returns None.
 
     Args:
-        lst (Sequence[T]): A sequence of elements.
+        lst (Sequence[Any]): A sequence of elements of any type.
 
     Returns:
-        Optional[T]: The first element of the sequence,
+        Union[Any, None]: The first element of the sequence,
         or None if the sequence is empty.
     """
     if lst:

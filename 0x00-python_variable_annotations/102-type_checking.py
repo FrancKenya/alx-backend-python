89e2b096082a774c0a4c0c21396747b9fd1e513b
Francis Waihiga <francisgwaihiga@gmail.com>
2024-12-03 23:42:05 +0300

    Task 12

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,30 +3,29 @@
 This module provides a type-annotated function
 """
 
-from typing import Sequence, List, Union
+from typing import Tuple, List
 
 
-def zoom_array(lst: Sequence, factor: Union[int, float] = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Zooms into an array by repeating each element `factor` times.
 
     Args:
-        lst (Sequence): The input sequence to zoom into.
-        factor (Union[int, float]): The number of
-        times to repeat each element.
+        lst (Tuple): The input tuple to zoom into.
+        factor (int): The number of times to repeat each element.
 
     Returns:
         List: A zoomed-in list with repeated elements.
     """
     zoomed_in: List = [
         item for item in lst
-        for i in range(int(factor))
+        for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)

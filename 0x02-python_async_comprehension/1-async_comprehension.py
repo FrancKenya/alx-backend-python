abeb1beaf183f0b3e9c60b7a4f018258ba8f5930
Francis Waihiga <francisgwaihiga@gmail.com>
2024-12-04 00:38:18 +0300

    Task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,7 @@ Asynchronous Comprehension
 """
 import asyncio
 from typing import List
-from async_generator import async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
